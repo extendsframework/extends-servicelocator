f80ec319ce841c235bfa405c7dca7a2b4858a086
Vincent van Dijk <info@vincentvandijk.com>
2017-10-16 18:46:06 +0200

    Updated description.

--- src/ServiceLocatorInterface.php
+++ src/ServiceLocatorInterface.php
@@ -9,7 +9,7 @@ interface ServiceLocatorInterface
      * Get a service with the name $key.
      *
      * A shared service will be created when $extra is null. If not, a managed service will be created. An exception
-     * will be thrown when $key is invalid or no service is found for $key.
+     * will be thrown when no service is found for $key or service is an non object.
      *
      * @param string $key
      * @param array  $extra
