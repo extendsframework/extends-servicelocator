4a1f087e98b7bf2b91cee44984d9bc3c7c755cd4
Vincent van Dijk <info@vincentvandijk.com>
2017-10-27 13:17:51 +0200

    Added service locator factory.

---
+++ src/ServiceLocatorFactory.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace ExtendsFramework\ServiceLocator;
+
+use ExtendsFramework\ServiceLocator\Exception\UnknownResolverType;
+use ExtendsFramework\ServiceLocator\Resolver\ResolverInterface;
+
+class ServiceLocatorFactory implements ServiceLocatorFactoryInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function create(array $config): ServiceLocatorInterface
+    {
+        $serviceLocator = new ServiceLocator($config);
+        foreach ($config[ServiceLocatorInterface::class] ?? [] as $fqcn => $services) {
+            $serviceLocator->addResolver(
+                $this->getResolver($fqcn, $services),
+                $fqcn
+            );
+        }
+
+        return $serviceLocator;
+    }
+
+    /**
+     * Get resolver with $name and register $services.
+     *
+     * An exception will be thrown when a resolver for $name can not be found.
+     *
+     * @param ResolverInterface|string $fqcn
+     * @param iterable                 $services
+     * @return ResolverInterface
+     * @throws UnknownResolverType
+     */
+    protected function getResolver(string $fqcn, iterable $services): ResolverInterface
+    {
+        if (is_subclass_of($fqcn, ResolverInterface::class, true) === false) {
+            throw new UnknownResolverType($fqcn);
+        }
+
+        return $fqcn::factory($services);
+    }
+}
---
+++ test/ServiceLocatorFactoryTest.php
@@ -0,0 +1,82 @@
+<?php
+declare(strict_types=1);
+
+namespace ExtendsFramework\ServiceLocator;
+
+use ExtendsFramework\ServiceLocator\Resolver\Alias\AliasResolver;
+use ExtendsFramework\ServiceLocator\Resolver\Closure\ClosureResolver;
+use ExtendsFramework\ServiceLocator\Resolver\Factory\FactoryResolver;
+use ExtendsFramework\ServiceLocator\Resolver\Factory\ServiceFactoryInterface;
+use ExtendsFramework\ServiceLocator\Resolver\Invokable\InvokableResolver;
+use ExtendsFramework\ServiceLocator\Resolver\Reflection\ReflectionResolver;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+class ServiceLocatorFactoryTest extends TestCase
+{
+    /**
+     * Create.
+     *
+     * Test that a ServiceLocatorInterface will be created from a config.
+     *
+     * @covers \ExtendsFramework\ServiceLocator\ServiceLocatorFactory::create()
+     * @covers \ExtendsFramework\ServiceLocator\ServiceLocatorFactory::getResolver()
+     */
+    public function testCreate(): void
+    {
+        $factory = new ServiceLocatorFactory();
+        $serviceLocator = $factory->create([
+            ServiceLocatorInterface::class => [
+                AliasResolver::class => [
+                    'foo' => 'bar'
+                ],
+                ClosureResolver::class => [
+                    'foo' => function () {
+                    }
+                ],
+                FactoryResolver::class => [
+                    'foo' => FactoryStub::class
+                ],
+                InvokableResolver::class => [
+                    'foo' => stdClass::class
+                ],
+                ReflectionResolver::class => [
+                    'foo' => stdClass::class
+                ]
+            ]
+        ]);
+
+        $this->assertInstanceOf(ServiceLocatorInterface::class, $serviceLocator);
+    }
+
+    /**
+     * Unknown resolver.
+     *
+     * Test that a unknown resolver can not be found.
+     *
+     * @covers                   \ExtendsFramework\ServiceLocator\ServiceLocatorFactory::create()
+     * @covers                   \ExtendsFramework\ServiceLocator\ServiceLocatorFactory::getResolver()
+     * @covers                   \ExtendsFramework\ServiceLocator\Exception\UnknownResolverType::__construct()
+     * @expectedException        \ExtendsFramework\ServiceLocator\Exception\UnknownResolverType
+     * @expectedExceptionMessage Resolver must be instance or subclass of ResolverInterface, got "A".
+     */
+    public function testUnknownResolver(): void
+    {
+        $factory = new ServiceLocatorFactory();
+        $factory->create([
+            ServiceLocatorInterface::class => [
+                'A' => []
+            ]
+        ]);
+    }
+}
+
+class FactoryStub implements ServiceFactoryInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function createService(string $key, ServiceLocatorInterface $serviceLocator, array $extra = null)
+    {
+    }
+}
